03d1dc183ce7dbad6183b69af4c91e85c010169a
Otto Seiskari <otto.seiskari@netmedi.fi>
2017-02-18 12:57:30 +0200

    more piece utils

--- game.js
+++ game.js
@@ -1,15 +1,5 @@
 "use strict";
 
-function Piece(blocks) {
-    this.blocks = blocks;
-}
-
-Piece.prototype.rotate = function(axis, sign) {
-    this.blocks.foreach(b => {
-        b.assign(rotate(b, axis, sign));
-    });
-};
-
 function Block(x, y, z) {
     this.x = x;
     this.y = y;
@@ -20,9 +10,11 @@ Block.prototype.assign = function(other) {
     this.x = other.x;
     this.y = other.y;
     this.z = other.z;
+    return this;
 };
 
-function rotate(block, axis, sign) {
+Block.prototype.rotated = function(axis, sign) {
+
     let exchangedAxes = {
         x: ['y', 'z'],
         y: ['z', 'x'],
@@ -33,8 +25,67 @@ function rotate(block, axis, sign) {
         throw "invalid axis "+axis;
     }
 
-    let b = new Block(block.x, block.y, block.z);
-    b[exchangedAxes[0]] = -sign*block[exchangedAxes[1]];
-    b[exchangedAxes[1]] = sign*block[exchangedAxes[0]];
+    let b = new Block(0,0,0).assign(this);
+    b[exchangedAxes[0]] = -sign*this[exchangedAxes[1]];
+    b[exchangedAxes[1]] = sign*this[exchangedAxes[0]];
     return b;
+};
+
+Block.prototype.translated = function(dx, dy, dz) {
+    return new Block(this.x + dx, this.y + dy, this.z + dz);
+};
+
+function Piece(blocks_) {
+    var center = {
+        x: 0,
+        y: 0,
+        z: 0
+    };
+
+    var blocks = blocks_.map(b => new Block(0,0,0).assign(b));
+
+    this.rotate = function(axis, sign) {
+        blocks.forEach(b => {
+            b.assign(b.rotated(axis, sign));
+        });
+    };
+
+    this.translateDict = function(d) {
+         ['x', 'y', 'z'].forEach(k => {
+            center[k] += d[k];
+        });
+    };
+
+    this.translate = function(dx, dy, dz) {
+        this.translateDict({x: dx, y: dy, z: dz});
+    };
+
+
+    this.getBlocks = function() {
+        return blocks.map(b => b.translated(center.x, center.y, center.z));
+    };
 }
+
+Piece.prototype.translateBeyond = function(axis, limit, dir) {
+    let func;
+    if (dir == 1) {
+        func = (a,b) => Math.min(a,b);
+    } else if (dir == -1) {
+        func = (a,b) => Math.max(a,b);
+    } else {
+        throw "invalid direction "+dir;
+    }
+
+    const extreme = this.getBlocks().map(b => b[axis]).reduce(func);
+
+    let diff = (limit - extreme)*dir;
+
+    if (diff > 0) {
+        let trans = {x: 0, y: 0, z: 0};
+        trans[axis] = diff*dir;
+        this.translateDict(trans);
+        return true;
+    }
+
+    return false;
+};
